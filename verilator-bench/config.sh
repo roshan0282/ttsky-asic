e6a264e73cfbe5f2949c3feb31a122ba98c85801
Ayan Ali <ayanali20985@gmail.com>
2026-02-16 18:40:53 -0500

    removed useless files

--- verilator-bench/config.sh
+++ verilator-bench/config.sh
@@ -3,10 +3,10 @@
 # edit these variables to point to your project
 
 # path to your verilog source folder
-export VERILATOR_SOURCE_FOLDER=/c/2w/ttsky-asic/raygaming-test
+export VERILATOR_SOURCE_FOLDER=/c/2w/ttsky-asic/raygaming-sphere-test
 
 # name of your top module
-export VERILATOR_TOP_MODULE=tt_um_tinytapeout_vga
+export VERILATOR_TOP_MODULE=vga_raytracer.sv
 
 # testbench to use (usually tb/tbGenericVga.v for tinytapeout projects)
 export VERILATOR_TESTBENCH=tb/tbGenericVga.v
