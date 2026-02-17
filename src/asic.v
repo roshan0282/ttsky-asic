c276acc3cd7587e286ed0256d98a2ee635fdf564
Ayan Ali <ayanali20985@gmail.com>
2026-02-16 20:06:15 -0500

    test the new text stuff2

--- src/asic.v
+++ src/asic.v
@@ -4,6 +4,7 @@ module asic(
 );
 wire [9:0] xOrd;
 wire [9:0] yOrd;
+wire [7:0] red, green, blue;
 
 timerCore core (
     .clk(1'b0),
@@ -11,9 +12,9 @@ timerCore core (
     .xOrd(10'd523),
     .yOrd(10'd187),
     .visible(1'b1),
-    .red(),
-    .green(),
-    .blue()
+    .red(red),
+    .green(green),
+    .blue(blue)
 );
 
 assign out [7:4] = xOrd [3:0];
